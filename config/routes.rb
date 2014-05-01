85de2a773e536ee9c3f15d21b6ae75a65e0e4845
Patrick Holloway <pholloway@patientslikeme.com>
2014-05-01 00:45:37 -0400

    some work on this here stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,14 @@
 Phphoto::Application.routes.draw do
   resources :posts
 
-  get "home/index"
-  
-  get "home/photo"
-  
-  get "home/web_design"
-  
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
+
+  get '/portfolio' => 'pages#portfolio'
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
@@ -57,7 +52,7 @@ Phphoto::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 

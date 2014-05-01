18454cd54b4244b0e0e0c77f233290f964a13ce8
Patrick Holloway <pholloway@patientslikeme.com>
2014-04-30 22:51:57 -0400

    first commit, very old project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phphoto::Application.config.secret_token = 'd7c7af53b79ed9e19aaf4d440f4f587dfbf287d86afdef224820ac0eb42bce42595eb9fc6341aaeb50cf1529dc47fa865a3fa200d111b7fea10a7d11f6b02104'

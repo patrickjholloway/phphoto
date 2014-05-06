dd2ca2831b1fd25fc04151c6879ce24bef7c602a
Patrick Holloway <pholloway@patientslikeme.com>
2014-05-06 19:39:57 -0400

    add accessors for paperclip properties

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,8 @@
 class Photo < ActiveRecord::Base
   has_attached_file :image, styles: {thumb: "x300>", sm_thumb: "x200>", sm_square: "100x100>", square: "300x300>"}
+  attr_accessor :image_file_name
+  attr_accessor :image_content_type
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
   belongs_to :gallery
 end
